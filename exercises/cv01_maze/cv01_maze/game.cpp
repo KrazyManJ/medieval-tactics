90933758e11ee45908269dd4019dc174c0e7c058
KrazyManJ <KrazyManJ@gmail.com>
2024-11-26 09:08:53 +0100

    Converted repo to medieval-tactics project, initialized qt quick project

--- exercises/cv01_maze/cv01_maze/game.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  game.cpp
-//  cv01_maze
-//
-//  Created by Jaroslav Korčák on 26.09.2024.
-//
-
-#include "game.hpp"
--- exercises/cv04_tesla/Maze/Position.h
+++
@@ -1,9 +0,0 @@
-#ifndef Position_h
-#define Position_h
-
-struct Position  {
-    int x;
-    int y;
-};
-
-#endif /* Position_h */
