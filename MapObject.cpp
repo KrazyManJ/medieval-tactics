9d11e825ae1679b501c955c505346de762988464
Miloslav svabensky <xsvaben4@mendelu.cz>
2024-11-30 10:22:21 +0100

    add MapObject .h

---
+++ MapObject.cpp
@@ -0,0 +1,5 @@
+//
+// Created by MiloslavŠvábenský(ml on 30.11.2024.
+//
+
+#include "MapObject.h"
---
+++ MapObject.h
@@ -0,0 +1,23 @@
+//
+// Created by MiloslavŠvábenský(ml on 30.11.2024.
+//
+
+#ifndef MAPOBJECT_H
+#define MAPOBJECT_H
+#include <GroundType.h>
+
+
+class MapObject {
+     bool m_solid;
+     GroundType m_GroundType;
+
+public:
+     MapObject(bool solid, GroundType groundtype);
+     GroundType getType();
+     bool isSolid();
+     ~MapObject();
+};
+
+
+
+#endif //MAPOBJECT_H
