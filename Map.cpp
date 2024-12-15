b49aad32b138203015d7ee39a6eb0959d3f0e2f9
Miloslav svabensky <xsvaben4@mendelu.cz>
2024-12-15 15:40:44 +0100

    fix for max

--- Map.cpp
+++ Map.cpp
@@ -17,7 +17,7 @@ int Map::getHeight() {
 }
 
 MapObject* Map::getObjectAt(int row, int column) {
-    if(isInRange(row,column)) {
+    if(!isInRange(row,column)) {
         throw std::out_of_range("Unit is out of map");
     }
     return mapMatrix.at(row).at(column);
