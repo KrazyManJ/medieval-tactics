d31bf2234ac5eca7156109d2c033930e15ebaa5d
Daniel Němec <daniel.nemec.0@gmail.com>
2025-01-08 16:08:14 +0100

    deleting autosave

--- UnitFactory.h
+++ UnitFactory.h
@@ -1,10 +1,16 @@
 #ifndef UNITFACTORY_H
 #define UNITFACTORY_H
 
+#include <memory>
+#include <string>
+#include "AttackUnit.h"
+#include "HealUnit.h"
+#include "BuildUnit.h"
+
 class UnitFactory
 {
 public:
-    UnitFactory();
+    static std::unique_ptr<Unit> createUnit(const std::string& unitType,int row,int column);
 };
 
 #endif // UNITFACTORY_H
