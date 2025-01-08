3e4b054098ce670514a5c1b3aa16da92c018a507
Daniel Němec <daniel.nemec.0@gmail.com>
2025-01-08 15:38:49 +0100

    creating unittype and unit factory

---
+++ UnitFactory.h
@@ -0,0 +1,10 @@
+#ifndef UNITFACTORY_H
+#define UNITFACTORY_H
+
+class UnitFactory
+{
+public:
+    UnitFactory();
+};
+
+#endif // UNITFACTORY_H
---
+++ UnitType.h
@@ -0,0 +1,12 @@
+#ifndef UNITTYPE_H
+#define UNITTYPE_H
+
+
+
+enum class UnitType {
+    Knight,
+    Priest,
+    Druid
+};
+
+#endif // UNITTYPE_H
