79ea59f1b5833455cd4e5dafd7d34c70776d0ecf
Miloslav Švábenský <xsvaben4@mendelu.cz>
2025-01-16 11:31:11 +0100

    shop.h, shop.cpp

--- Shop.cpp
+++ Shop.cpp
@@ -1,3 +1,39 @@
 #include "Shop.h"
 
-Shop::Shop() {}
+Shop::Shop() {
+    isFirstPlayerReady = false;
+    isSecondPlayerReady = false;
+}
+
+void Shop::buyUnitForCurrentPlayer(std::string unitID, int row, int column) {
+    Game* game = Game::getInstance();
+
+
+
+    Unit* unit = UnitFactory::createUnit(unitID, row, column);
+    if(isFirstPlayerReady == false) {
+        Player* player = game->getFirstPlayer();
+        player->addUnit(unit);
+    } else {
+        Player* player = game->getSecondPlayer();
+        player->addUnit(unit);
+    }
+}
+
+
+bool Shop::isReady() {
+    if(isFirstPlayerReady == true && isSecondPlayerReady == true) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void Shop::markReady() {
+    if(isFirstPlayerReady == false) {
+        isFirstPlayerReady = true;
+    } else {
+        isSecondPlayerReady = true;
+    }
+}
+
--- Shop.h
+++ Shop.h
@@ -1,10 +1,18 @@
 #ifndef SHOP_H
 #define SHOP_H
+#include "UnitFactory.h"
+#include "Player.h"
 
-class Shop
-{
+class Shop {
+private:
+    bool isFirstPlayerReady;
+    bool isSecondPlayerReady;
 public:
     Shop();
+    void buyUnitForCurrentPlayer(std::string unitID, int row, int column);
+    bool isReady();
+    void markReady();
+    ~Shop();
 };
 
 #endif // SHOP_H
