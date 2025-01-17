e51444861934fad2f650cbb8a86e874ff936d01d
Daniel Němec <daniel.nemec.0@gmail.com>
2025-01-17 16:36:35 +0100

    chaning description of units

--- UnitFactory.cpp
+++ UnitFactory.cpp
@@ -9,12 +9,12 @@ Unit* UnitFactory::createUnit(const std::string& unitType,int row,int column){
     } else if (unitType == "Priest") {
         return new HealUnit (
             80, 5, 2, 6, 4, std::vector<GroundType>{GroundType::Grass},
-            Position{.row=row,.column=column}, 10, "Knight", "A brave knight."
+            Position{.row=row,.column=column}, 10, "Priest", "A brave priest."
             );
     } else if (unitType == "Druid") {
         return new BuildUnit (
             90, 7, 3, 3, 6, std::vector<GroundType>{GroundType::Grass},
-            Position{.row=row,.column=column}, 10, "Knight", "A brave knight."
+            Position{.row=row,.column=column}, 10, "Druid", "A wierd fella."
             );
     } else {
         throw std::invalid_argument("Unknown unit type!");
